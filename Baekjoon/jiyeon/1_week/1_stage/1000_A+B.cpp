d4c9ba387d51c3921e72421baa360075169f7231
Bosomi <she79560@naver.com>
2022-03-15 23:57:34 +0900

    [백준 - 스테이지 1문제 2번~8번 ](-지연)

---
+++ Baekjoon/jiyeon/1_week/1_stage/1000_A+B.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a+b);
+    return 0;
+}
+
+/* iostream사용하는 방법
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+} */
