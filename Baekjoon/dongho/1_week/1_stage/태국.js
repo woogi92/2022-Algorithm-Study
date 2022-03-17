6e9accbcf65b535c6b1cdaa868c13b2009ddc3a4
Jang-DongHo <donghoo18@naver.com>
2022-03-17 20:47:54 +0900

    백준 7번~13번 문제 추가

---
+++ Baekjoon/dongho/1_week/1_stage/태국.js
@@ -0,0 +1,9 @@
+let fs = require("fs");
+let input = fs
+  .readFileSync(
+    "/Users/dongho/Desktop/Github/2022-Algorithm-Study/Baekjoon/dongho/input.txt"
+  )
+  .toString();
+let a = parseInt(input);
+
+console.log(a - 543);
