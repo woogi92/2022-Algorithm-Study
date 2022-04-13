5edb7f4281d5bbcfde7513e4f2d33bf2100a5dfb
Bosomi <she79560@naver.com>
2022-04-13 20:25:05 +0900

    [백준-스테이지6(문자열)] 6~7문제 주석 추가 (지연)

--- Baekjoon/jiyeon/4_week/6_stage/1152_단어의개수.cpp
+++ Baekjoon/jiyeon/4_week/6_stage/1152_단어의개수.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 int main()
 {
-    string input;
-    int wordCount = 0;
+    string input;           // 입력받을 문자열
+    int wordCount = 0;      // 단어 개수
 
-    getline(cin, input);
+    getline(cin, input);    // 입력
 
+    // 입력받은 문자열이 공백일 경우, 0개만 출력
     if(input.empty())
     {
         cout << wordCount;
@@ -19,6 +20,7 @@ int main()
 
     wordCount = 1;
 
+    // 입력받은 문자열에 공백이 있으면 wordCount 증가
     for(int i = 0; i < input.length(); i++)
     {
         if(input.at(i) == ' ')
@@ -27,6 +29,7 @@ int main()
         }
     }
 
+    // 입력받은 문자열의 처음과 끝에 공백이 있을 경우 wordCount 감소
     if(input[0] == ' ')
     {
         wordCount--;
@@ -36,6 +39,7 @@ int main()
         wordCount--;
     }
 
+    // 결과 출력
     cout << wordCount;
     return 0;
 }
--- Baekjoon/jiyeon/4_week/6_stage/2908_상수.cpp
+++ Baekjoon/jiyeon/4_week/6_stage/2908_상수.cpp
@@ -6,14 +6,16 @@ using namespace std;
 
 int main()
 {
-    string input;
-    string tmp = "" ;
-    int num[2] = {0,0};
+    string input;           // 입력받은 문자열
+    int num[2] = {0,0};     // 상수가 읽는 숫자
 
+    // 숫자 입력 받기
     for(int i = 0; i < 2; i++)
     {
         cin >> input;
-        int digit = 2;
+
+        int digit = 2;  // 자리수
+        // 입력받은 문자열을 상수가 읽는 숫자로 변경
         for(int j = input.length()-1; j >= 0; j--)
         {
             num[i] += ((int)input.at(j) - 48) * pow(10,digit);
@@ -21,6 +23,7 @@ int main()
         }
     }
 
+    // 상수가 읽은 숫자 중 큰 값 출력
     if(num[0] < num[1])
     {
         cout << num[1];
