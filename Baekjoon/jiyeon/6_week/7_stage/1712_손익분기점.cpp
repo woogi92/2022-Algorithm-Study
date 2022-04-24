30bb1a2b01dfb1b966508356bc5f13ea1bbc4a69
Bosomi <she79560@naver.com>
2022-04-24 21:26:17 +0900

    [백준-스테이지7(기본수학)] 1712_손익분기점 (지연)

---
+++ Baekjoon/jiyeon/6_week/7_stage/1712_손익분기점.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+// 손익분기점을 얻는 함수
+int GetBreakEvenPoint(int fixedCost, int variableCost, int price)
+{
+    int count = fixedCost;                          // 노트북 판매 수
+
+    if(price - variableCost <= 0)
+    {
+        count = -1;
+    }
+
+    // 손익분기점 = A / (C-B)  +1
+    else 
+    {
+        count =  fixedCost / (price - variableCost) + 1;
+    }
+
+    return count;
+}
+
+int main()
+{
+    int fixedCost;       // 고정비용
+    int variableCost;    // 가변비용
+    int price;           // 노트북 한 대 가격
+
+    cin >> fixedCost >> variableCost >> price;
+
+    cout << GetBreakEvenPoint(fixedCost, variableCost, price);
+
+    return 0;
+}
