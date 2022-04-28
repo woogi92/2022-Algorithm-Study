8ad32c08ac73d9674b394d0ff55a365f3dbc2daa
Bosomi <she79560@naver.com>
2022-04-28 19:58:37 +0900

    [백준-스테이지7(기본수학1)] 2869_달팽이는올라가고싶다 (지연)

---
+++ Baekjoon/jiyeon/6_week/7_stage/2869_달팽이는올라가고싶다.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, v;
+
+    cin >> a >> b >> v;
+
+    int day = (v - b - 1) / (a - b) + 1;
+
+    /* 풀려했지만 실패한 내 코드
+    int up = a * day - b * (day -1); // 전날 올라간 거리
+   
+    while(1)
+    {
+        if(up >= v)
+        {
+            break;
+        }
+        up -= b;
+        day++;
+        up += a;        
+    }
+    */
+  
+    cout << day;
+    return 0;
+}
