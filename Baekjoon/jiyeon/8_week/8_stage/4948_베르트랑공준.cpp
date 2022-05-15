a91d0067bbaadf5770d9cc907a7fcec46feb4470
Bosomi <she79560@naver.com>
2022-05-15 20:10:14 +0900

    [백준-스테이지8] 9020_골드바흐의추측(지연)

--- Baekjoon/jiyeon/8_week/8_stage/4948_베르트랑공준.cpp
+++ Baekjoon/jiyeon/8_week/8_stage/4948_베르트랑공준.cpp
@@ -50,24 +50,24 @@ int main()
         cnt = 0;
         
         for(int i=M; i<=N; i++){
-        root = sqrt(i);
-        //2또는 3일 경우
-        if(root == 1 && i != 1){
-            cnt++;
-            continue;
-        }
-        //n보다 크고 2보다 작거나 같은 수가 홀수일 경우(짝수이면 소수가 아니므로)
-        if(i%2){
-            for(int j=2; j<=root; j++){
-                //나머지가 0이면 소수가 아니다. 종료.
-                if(!(i%j))
-                    break;
-                //바깥에 빼도 되지만 뺄라면 j값도 알고 있어야 함.
-                if(j == root){
-                    cnt++;
+            root = sqrt(i);
+            //2또는 3일 경우
+            if(root == 1 && i != 1){
+                cnt++;
+                continue;
+            }
+            //n보다 크고 2보다 작거나 같은 수가 홀수일 경우(짝수이면 소수가 아니므로)
+            if(i%2){
+                for(int j=2; j<=root; j++){
+                    //나머지가 0이면 소수가 아니다. 종료.
+                    if(!(i%j))
+                        break;
+                    //바깥에 빼도 되지만 뺄라면 j값도 알고 있어야 함.
+                    if(j == root){
+                        cnt++;
+                    }
                 }
             }
-        }
         }
         cout<<cnt<<'\n';
     }  
---
+++ Baekjoon/jiyeon/9_week/8_stage/9020_골드바흐의추측.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool CheckPrimeNum(int num)
+{
+  int i = 2, root;
+  root = sqrt(num);
+
+  if(root == 1)
+    return true;
+
+  if(num%2)
+  {
+    for(; i<=root; i++)
+    {
+        if(!(num%i))
+            return false;      
+        if(i == root)
+            return true;
+    }
+  }
+  return false;
+}
+
+int main()
+{
+    int T, i;
+    cin >> T;
+
+    for(; i++<T;)
+    {
+        int n, a, b, half;
+
+        cin >> n;
+        half = n/2;
+
+        for(int j = half; j >= 2; j--)
+        {
+            if(CheckPrimeNum(j) && CheckPrimeNum(n-j))
+            {
+                cout << j << " " << n-j << '\n';       
+                break;
+            } 
+        }
+    }  
+}
