859e386793151e0702d0e95f2609758a6ed0ebaa
Jang-DongHo <donghoo18@naver.com>
2022-05-26 19:49:14 +0900

    Create 문자열뒤집기.js

---
+++ Baekjoon/dongho/10_week/문자열뒤집기.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(
+    "/Users/dongho/Desktop/Github/2022-Algorithm-Study/Baekjoon/dongho/input/input.txt"
+  )
+  .toString();
+
+const reverseString = (str) => {};
+
+reverseString(input);
