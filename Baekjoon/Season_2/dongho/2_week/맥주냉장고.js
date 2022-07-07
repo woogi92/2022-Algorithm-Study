232dc4105b2e728752b5edc702f03c9f55e54610
Jang-DongHo <donghoo18@naver.com>
2022-07-07 16:26:53 +0900

    맥주냉장고.js 저장

--- Baekjoon/Season_2/dongho/2_week/맥주냉장고.js
+++ Baekjoon/Season_2/dongho/2_week/맥주냉장고.js
@@ -15,6 +15,10 @@ const isPrime = (num) => {
 };
 
 const getMinPrime = (num) => {
+  // 입력값이 1일 경우
+  if (num === 1) {
+    return 1;
+  }
   for (let i = 2; i <= num; i++) {
     if (isPrime(i) && num % i === 0) {
       return i;
@@ -23,21 +27,36 @@ const getMinPrime = (num) => {
 };
 
 const distributeSize = (size) => {
-  if (size === tmpSize) {
-    return size;
-  }
-  const minPrime = getMinPrime(size[0]);
-  size[0] /= minPrime;
+  const minPrime = getMinPrime(size[check]);
+  size[check] /= minPrime;
   size[2] *= minPrime;
   size.sort((a, b) => b - a);
-  if (history.lastIndexOf(size)) {
-    return;
+  for (let i = history.length; i > 0; i--) {
+    if (JSON.stringify(size) === JSON.stringify(history[i])) {
+      let output;
+      let minSize = 10000000;
+      history.forEach((size) => {
+        const tmpSize = size[0] + size[1] + size[2];
+        if (tmpSize < minSize) {
+          minSize = tmpSize;
+          output = size;
+        }
+      console.log(output.join(" "));
+      return;
+      } else {
+        check = 1;
+        history.push(size.slice());
+        console.log(history);
+        distributeSize(size);
+      }
+    }
   }
+  history.push(size.slice());
   distributeSize(size);
 };
 
+let check = 0;
 let size = [+input, 1, 1];
-let history = [];
+let history = [size.slice()];
 
 distributeSize(size);
-console.log(size.join(" "));
