7190d90b9dee25e5a4e22cbd68798dca54e94cb5
Jang-DongHo <donghoo18@naver.com>
2022-07-07 19:59:18 +0900

    Update 암호해독.js

--- Baekjoon/Season_2/dongho/2_week/암호해독.js
+++ Baekjoon/Season_2/dongho/2_week/암호해독.js
@@ -33,23 +33,29 @@ const ASCIItoString = (ASCII) => {
   return ASCII.join("").trim();
 };
 
-let password = stringToASCII(input[0]);
-const N = +input[1];
-let words = [];
-let isFind = false;
+let password = stringToASCII(input[0]); // 암호문
+const N = +input[1]; // 사전에 있는 단어의 수
+let words = []; // 단어들
+let isFind = false; // 단어가 존재하는지 여부
+// 단어 저장
 for (let i = 2; i < N + 2; i++) {
   words.push(stringToASCII(input[i]));
 }
-for (let i = 0; i < 26; i++) {
+
+// 단어를 찾을 때까지 무한 반복
+while (1) {
+  // 암호문과 단어 비교
   for (let i = 0; i < N; i++) {
     if (isInclude(words[i])) {
       isFind = true;
       break;
     }
   }
+  // 단어를 찾았을 경우 반복문 탈출
   if (isFind) {
     break;
   }
+  // 단어를 못 찾았을 경우 알파벳 칸 변경
   password = shiftASCII(password);
 }
 
