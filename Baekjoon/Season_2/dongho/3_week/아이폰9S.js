7b9b458e1d952e01425887036929e80aafe1c464
Jang-DongHo <donghoo18@naver.com>
2022-07-28 16:23:36 +0900

    아이폰9S.js

--- Baekjoon/Season_2/dongho/3_week/아이폰9S.js
+++ Baekjoon/Season_2/dongho/3_week/아이폰9S.js
@@ -9,15 +9,16 @@ let input = fs
 
 const clientsNum = input.shift();
 const clients = input;
-let maxLength = 0;
+let maxLength = 1;
 
 const removeClients = (clients, GB) => {
   return clients.filter((x) => x !== GB);
 };
 
 const searchMaxLength = (clients) => {
-  let count = 0;
+  let count = 1;
   for (let i = 0; i < clients.length; i++) {
+    // 배열의 마지막 인덱스에 도달했을 경우 break
     if (i + 1 === clients.length) {
       break;
     }
@@ -27,28 +28,25 @@ const searchMaxLength = (clients) => {
         maxLength = count;
       }
     } else {
-      count = 0;
+      count = 1;
     }
   }
 };
 
 const main = () => {
-  let GBArray = [];
+  let GBArray = []; // 처리했던 용량을 담는 배열
   //배열 전체 탐색
   for (let i = 0; i < clientsNum; i++) {
     const GB = clients[i];
+    //처리했던 용량이라면 continue
     if (GBArray.includes(GB)) {
       continue;
     }
-    const tmpClients = removeClients(clients, GB);
-    searchMaxLength(tmpClients);
-    GBArray.push(GB);
+    const tmpClients = removeClients(clients, GB); // 특정 용량을 제외한 배열
+    searchMaxLength(tmpClients); // 배열 길이 탐색
+    GBArray.push(GB); // 처리한 용량은 배열에 push
   }
-  if (maxLength) {
-    console.log(maxLength + 1);
-  } else {
-    console.log(0);
-  }
+  console.log(maxLength);
 };
 
 main();
