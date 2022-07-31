c035798ff57fa0bb5d6e429b44e8bbf7750b3336
Jang-DongHo <donghoo18@naver.com>
2022-07-31 23:09:43 +0900

    사탕게임.js

--- Baekjoon/Season_2/dongho/3_week/사탕게임.js
+++ Baekjoon/Season_2/dongho/3_week/사탕게임.js
@@ -1,4 +1,5 @@
 let fs = require("fs");
+const { serialize } = require("v8");
 let input = fs
   .readFileSync(
     "/Users/dongho/Desktop/Github/2022-Algorithm-Study/Baekjoon/Season_2/dongho/input.txt"
@@ -13,20 +14,55 @@ for (let i = 0; i < N; i++) {
   const tmp = inputBoards[i].split("");
   boards.push(tmp);
 }
+let maxLength = 0;
 
-const checkContinuity = (boards) => {};
+const checkRow = (boards, i, j) => {
+  let count = 1;
+  for (let j = 0; j < N; j++) {
+    if (j + 1 === N) break;
+    if (boards[i][j] === boards[i][j + 1]) {
+      count++;
+      if (count > maxLength) {
+        maxLength = count;
+      }
+    } else count = 1;
+  }
+};
+
+const checkColumn = (boards, i, j) => {
+  let count = 1;
+  for (let i = 0; i < N; i++) {
+    if (i + 1 === N) break;
+    if (boards[i][j] === boards[i + 1][j]) {
+      count++;
+      if (count > maxLength) {
+        maxLength = count;
+      }
+    } else count = 1;
+  }
+};
+
+const checkContinuity = (boards, i, j) => {
+  const tmpBoards = boards.map((candy) => candy.slice()); // 2차원 배열 복사
+  const tmpCandy = tmpBoards[i][j];
+  tmpBoards[i][j] = tmpBoards[i][j + 1];
+  tmpBoards[i][j + 1] = tmpCandy;
+
+  //행 탐색
+  checkRow(tmpBoards, i, j);
+  checkColumn(tmpBoards, i, j);
+};
 
 // 행 순차탐색
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
-    if (j + 1 === N) {
-      break;
-    }
-    if (boards[i][j] === boards[i][j + 1]) {
-      continue;
-    }
-    checkContinuity(boards);
+    if (j + 1 === N) break;
+    checkContinuity(boards, i, j);
   }
 }
 
+console.log(maxLength);
+
 // 열 순차탐색
+
+//https://intrepidgeeks.com/tutorial/copy-onedimensional-and-twodimensional-arrays
