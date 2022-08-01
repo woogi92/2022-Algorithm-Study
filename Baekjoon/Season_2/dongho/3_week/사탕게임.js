107baf7726293630a63e8f998f335cd5d9f2e25a
Jang-DongHo <donghoo18@naver.com>
2022-08-01 20:37:10 +0900

    사탕게임.js

--- Baekjoon/Season_2/dongho/3_week/사탕게임.js
+++ Baekjoon/Season_2/dongho/3_week/사탕게임.js
@@ -1,5 +1,4 @@
 let fs = require("fs");
-const { serialize } = require("v8");
 let input = fs
   .readFileSync(
     "/Users/dongho/Desktop/Github/2022-Algorithm-Study/Baekjoon/Season_2/dongho/input.txt"
@@ -7,62 +6,79 @@ let input = fs
   .toString()
   .split("\n");
 
-const N = +input.shift();
-const inputBoards = [...input];
-const boards = [];
-for (let i = 0; i < N; i++) {
-  const tmp = inputBoards[i].split("");
-  boards.push(tmp);
-}
-let maxLength = 0;
-
 const checkRow = (boards, i, j) => {
-  let count = 1;
-  for (let j = 0; j < N; j++) {
-    if (j + 1 === N) break;
-    if (boards[i][j] === boards[i][j + 1]) {
-      count++;
-      if (count > maxLength) {
-        maxLength = count;
+  for (let i = 0; i < N; i++) {
+    let count = 1;
+    for (let j = 0; j < N - 1; j++) {
+      if (boards[i][j] === boards[i][j + 1]) {
+        count++;
+        if (count > maxLength) {
+          maxLength = count;
+        }
+      } else {
+        count = 1;
       }
-    } else count = 1;
+    }
   }
 };
 
 const checkColumn = (boards, i, j) => {
-  let count = 1;
-  for (let i = 0; i < N; i++) {
-    if (i + 1 === N) break;
-    if (boards[i][j] === boards[i + 1][j]) {
-      count++;
-      if (count > maxLength) {
-        maxLength = count;
+  for (let j = 0; j < N; j++) {
+    let count = 1;
+    for (let i = 0; i < N - 1; i++) {
+      if (boards[i][j] === boards[i + 1][j]) {
+        count++;
+        if (count > maxLength) {
+          maxLength = count;
+        }
+      } else {
+        count = 1;
       }
-    } else count = 1;
+    }
   }
 };
 
-const checkContinuity = (boards, i, j) => {
+const checkContinuity = (boards, type, i, j) => {
   const tmpBoards = boards.map((candy) => candy.slice()); // 2차원 배열 복사
   const tmpCandy = tmpBoards[i][j];
-  tmpBoards[i][j] = tmpBoards[i][j + 1];
-  tmpBoards[i][j + 1] = tmpCandy;
-
-  //행 탐색
+  // 행 캔디 바꾸기
+  if (type === "row") {
+    tmpBoards[i][j] = tmpBoards[i][j + 1];
+    tmpBoards[i][j + 1] = tmpCandy;
+  }
+  // 열 캔디 바꾸기
+  if (type === "column") {
+    tmpBoards[i][j] = tmpBoards[i + 1][j];
+    tmpBoards[i + 1][j] = tmpCandy;
+  }
+  // 행열 탐색 후 사탕 최대 개수 찾기
   checkRow(tmpBoards, i, j);
   checkColumn(tmpBoards, i, j);
 };
 
-// 행 순차탐색
+const N = +input.shift();
+const inputBoards = [...input];
+let boards = [];
 for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (j + 1 === N) break;
-    checkContinuity(boards, i, j);
+  const tmp = inputBoards[i].split("");
+  boards.push(tmp);
+}
+let maxLength = 1;
+
+// 행 탐색
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N - 1; j++) {
+    checkContinuity(boards, "row", i, j);
+  }
+}
+
+// 열 탐색
+for (let j = 0; j < N; j++) {
+  for (let i = 0; i < N - 1; i++) {
+    checkContinuity(boards, "column", i, j);
   }
 }
 
 console.log(maxLength);
-
-// 열 순차탐색
 
 //https://intrepidgeeks.com/tutorial/copy-onedimensional-and-twodimensional-arrays
