194fa2bfa98f32609bd3f11d8e876fb839484f9b
Jang-DongHo <donghoo18@naver.com>
2022-11-09 15:50:19 +0900

    update

--- Baekjoon/Season_3/dongho/1_week/insertEraseArray.c
+++ Baekjoon/Season_3/dongho/1_week/insertEraseArray.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 
+int is_full(int* len) {
+  printf("%d", *len);
+  return *len > 10;
+}
+
 void insert(int idx, int num, int arr[], int* len){
-	
+	printf("%d", *len);
 }
 
 void erase(int idx, int arr[], int* len){
   
 }
 
-void printArr(int arr[], int* len){
+void printArr(int arr[], int len){
   for(int i = 0; i < len; i++) {
-		printf(arr[i] + " ");
+		printf("%d ", arr[i]);
 	}
 }
 
 void insert_test(){
   int arr[10] = {10, 20, 30};
   int len = 3;
-  insert(3, 40, arr, &len); // 10 20 30 40
-  printArr(arr, len);
+  int *p_len = &len;
+  insert(3, 40, arr, &p_len); // 10 20 30 40
+  //printArr(arr, len);
+  /*
   insert(1, 50, arr, &len); // 10 50 20 30 40
   printArr(arr, len);
   insert(0, 15, arr, &len); // 15 10 50 20 30 40
   printArr(arr, len);
+  */
 }
-
+/*
 void erase_test(){
   int arr[10] = {10, 50, 40, 30, 70, 20};
   int len = 6;
@@ -35,7 +43,7 @@ void erase_test(){
   erase(3, arr, len); // 10 40 30
   printArr(arr, len);
 }
-
+*/
 int main(void) {
   insert_test();
   //erase_test();
