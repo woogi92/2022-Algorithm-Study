527424d0508661e997e3a1bbdd69d25a1fe02221
woogi92 <oogi92@naver.com>
2023-01-15 18:54:09 +0900

    BOJ_10866

---
+++ Baekjoon/Season_3/wonwook/5_week/BOJ_10866.java
@@ -0,0 +1,103 @@
+package deque;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_10866 {
+    public static void main(String []args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer sToken;
+        sToken = new StringTokenizer(reader.readLine());
+        int command = Integer.parseInt(sToken.nextToken());
+        int arr[] = new int[2*command+1];
+        int head, tail, size, totalSize;
+        head = command;
+        tail = command;
+        totalSize = 2*command+1;
+        size = tail-head;
+        for(int i=0; i<totalSize; i++) {
+            arr[i] = -1;
+        }
+
+        for(int i=0; i<command; i++){
+            sToken = new StringTokenizer(reader.readLine());
+            String str = sToken.nextToken();
+            int num;
+            switch(str){
+                case "push_front":
+                    num = Integer.parseInt(sToken.nextToken());
+                    if(arr[head] == -1){
+                        arr[head--] = num;
+                    } else{
+                        arr[--head] = num;
+                    }
+                    size = tail-head;
+                    break;
+                case "push_back":
+                    num = Integer.parseInt(sToken.nextToken());
+                    if(arr[tail] == -1){
+                        arr[tail++] = num;
+                    } else{
+                        arr[++tail] = num;
+                    }
+                    size = tail-head;
+                    break;
+                case "pop_front":
+                    if(size <= 0) writer.write(-1 + "\n");
+                    else {
+                        if(arr[head] == -1){
+                            writer.write(arr[++head] + "\n");
+                            arr[head] = -1;
+                        } else{
+                            writer.write(arr[head] + "\n");
+                            arr[head++] = -1;
+                        }
+                    }
+                    size = tail-head;
+                    break;
+                case "pop_back":
+                    if(size <= 0) writer.write(-1 + "\n");
+                    else {
+                        if(arr[tail] == -1){
+                            writer.write(arr[--tail] + "\n");
+                            arr[tail] = -1;
+                        } else{
+                            writer.write(arr[tail] + "\n");
+                            arr[tail--] = -1;
+                        }
+                    }
+                    size = tail-head;
+                    break;
+                case "size":
+                    writer.write(size + "\n");
+                    break;
+                case "empty":
+                    if(size == 0) writer.write(1 + "\n");
+                    else writer.write(0 + "\n");
+                    break;
+                case "front":
+                    if(size <= 0) writer.write(-1 + "\n");
+                    else {
+                        if(arr[head] == -1){
+                            writer.write(arr[head+1] + "\n");
+                        } else{
+                            writer.write(arr[head] + "\n");
+                        }
+                    }
+                    break;
+                case "back":
+                    if(size <= 0) writer.write(-1 + "\n");
+                    else {
+                        if(arr[tail] == -1){
+                            writer.write(arr[tail-1] + "\n");
+                        } else{
+                            writer.write(arr[tail] + "\n");
+                        }
+                    }
+                    break;
+            }
+        }
+        writer.flush();
+    }
+}
